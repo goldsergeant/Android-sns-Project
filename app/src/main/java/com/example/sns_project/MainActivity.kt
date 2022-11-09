f27565958f8714cf903696fc9280beb999a6705b
goldsergeant <94740533+goldsergeant@users.noreply.github.com>
2022-11-09 22:55:12 +0900

    로그인 액티비티

--- app/src/main/java/com/example/sns_project/MainActivity.kt
+++ app/src/main/java/com/example/sns_project/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.sns_project
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.sns_project.databinding.ActivityMainBinding
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        if (Firebase.auth.currentUser == null) {
+            startActivity(
+                Intent(this, LoginActivity::class.java)
+            )
+            finish()
+        }
+
     }
 }
