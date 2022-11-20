a8e138749d910e99c114c0a39a9e62d1458bf0a9
hyunpjh <qkrwlgus4001@naver.com>
2022-11-20 09:53:32 +0900

    하단바 방식 변경, post일부 구현(사진만 업로드)

--- app/src/main/java/com/example/sns_project/MainActivity.kt
+++ app/src/main/java/com/example/sns_project/MainActivity.kt
@@ -3,19 +3,16 @@ package com.example.sns_project
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.fragment.app.Fragment
 import androidx.transition.Scene
 import com.example.sns_project.databinding.ActivityMainBinding
+import com.google.android.material.navigation.NavigationView
 import com.google.android.material.tabs.TabLayout
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var scene_friendList: ListFriend
-    private lateinit var scene_post: Post
-    private lateinit var scene_home: Home
-    private lateinit var scene_search: SearchFriend
-    private lateinit var scene_profile: Profile
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,43 +20,31 @@ class MainActivity : AppCompatActivity() {
         val binding=ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val tabBar=binding.tabbar
-        scene_friendList=ListFriend()
-        scene_post=Post()
-        scene_home=Home()
-        scene_search=SearchFriend()
-        scene_profile= Profile()
-        supportFragmentManager.beginTransaction().add(R.id.framelayout,scene_home).commit()
-
-        tabBar.addOnTabSelectedListener(object :TabLayout.OnTabSelectedListener{
-            override fun onTabSelected(tab: TabLayout.Tab?) {
-                when(tab?.position){
-                    0->{
-                        replaceView(scene_home)
-                    }
-                    1->{
-                        replaceView(scene_search)
-                    }
-                    2->{
-                        replaceView(scene_post)
-                    }
-                    3->{
-                        replaceView(scene_friendList)
-                    }
-                    4->{
-                        replaceView(scene_profile)
-                    }
+        binding.bottomNavigation.setOnItemSelectedListener { item->
+            when(item.itemId){
+                R.id.home->{
+                    var HomeFragment = Home()
+                    supportFragmentManager.beginTransaction().replace(R.id.framelayout, HomeFragment).commit()
+                }
+                R.id.search->{
+                    var SearchFragment = SearchFriend()
+                    supportFragmentManager.beginTransaction().replace(R.id.framelayout, SearchFragment).commit()
+                }
+                R.id.post->{
+                    startActivity(Intent(this,Post::class.java))
+                }
+                R.id.friend->{
+                    var FriendFragment = ListFriend()
+                    supportFragmentManager.beginTransaction().replace(R.id.framelayout, FriendFragment).commit()
+                }
+                R.id.profile->{
+                    var ProfileFragment = Profile()
+                    supportFragmentManager.beginTransaction().replace(R.id.framelayout, ProfileFragment).commit()
                 }
             }
+            true
+        }
 
-            override fun onTabUnselected(tab: TabLayout.Tab?) {
-
-            }
-
-            override fun onTabReselected(tab: TabLayout.Tab?) {
-
-            }
-        })
 
         if (Firebase.auth.currentUser == null) {
             startActivity(
@@ -69,11 +54,5 @@ class MainActivity : AppCompatActivity() {
         }
 
     }
-    private fun replaceView(tab:Fragment){
-        var selectedFragment:Fragment?=null
-        selectedFragment=tab
-        selectedFragment.let{
-            supportFragmentManager.beginTransaction().replace(R.id.framelayout,it).commit()
-        }
-    }
+
 }
--- app/src/main/java/com/example/sns_project/Post.kt
+++ app/src/main/java/com/example/sns_project/Post.kt
@@ -1,59 +1,110 @@
 package com.example.sns_project
 
+import android.Manifest
+import android.app.Activity
+import android.content.ContentUris
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.BitmapFactory
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.ImageView
+import androidx.appcompat.app.AlertDialog
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.transition.Scene
+import com.example.sns_project.databinding.ActivityLoginBinding
+import com.example.sns_project.databinding.ActivityPostBinding
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.tabs.TabLayout
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
+import com.google.firebase.storage.ktx.storage
+import java.text.SimpleDateFormat
+import java.util.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+class Post : AppCompatActivity() {
 
-/**
- * A simple [Fragment] subclass.
- * Use the [Post.newInstance] factory method to
- * create an instance of this fragment.
- */
-class Post : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    lateinit var storage: FirebaseStorage
+    lateinit var binding: ActivityPostBinding
+
+    var PICK_IMAGE_FROM_ALBUM = 0
+    var photoUri : Uri? = null
+
+    lateinit var storagePermission: ActivityResultLauncher<String>
+
+    lateinit var gallaryLauncher: ActivityResultLauncher<String>
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+        binding = ActivityPostBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        Firebase.auth.currentUser ?: finish()
+
+        storage = Firebase.storage
+
+        storagePermission = registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted->
+            if(isGranted) {
+                setViews()
+            } else {
+                Toast.makeText(baseContext,"외부저장소 권한을 승인해주십시오.",Toast.LENGTH_LONG).show()
+                finish()
+            }
+        }
+
+        gallaryLauncher = registerForActivityResult(ActivityResultContracts.GetContent()) { uri->
+            binding.postPhoto.setImageURI(uri)
+            photoUri = uri
+        }
+
+
+        storagePermission.launch(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+    }
+
+    /*override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        val binding = ActivityPostBinding.inflate(layoutInflater)
+        if(requestCode == PICK_IMAGE_FROM_ALBUM){
+            if(resultCode == Activity.RESULT_OK){
+                photoUri = data?.data
+                binding.postPhoto.setImageURI(photoUri)
+            }else{
+                finish()
+            }
+        }
+    }*/
+
+    fun setViews() {
+        binding.postPhotobutton.setOnClickListener {
+            /*var photoPickerIntent = Intent(Intent.ACTION_PICK)
+            photoPickerIntent.type = "image/*"
+            startActivityForResult(photoPickerIntent,PICK_IMAGE_FROM_ALBUM)*/*/
+            gallaryLauncher.launch("image/*")
+        }
+
+        binding.postUploadbutton.setOnClickListener {
+            contentUpload(photoUri!!)
         }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.activity_post, container, false)
-    }
+    fun contentUpload(photoUri: Uri){
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment Post.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            Post().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        var timestamp = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+        var imageFileName = "IMAGE_" + timestamp + "_.png"
+
+        var storageRef = storage?.reference?.child("images")?.child(imageFileName)
+
+        storageRef?.putFile(photoUri)?.addOnSuccessListener {
+            Toast.makeText(this,"upload success",Toast.LENGTH_LONG).show()
+        }
+
     }
 }
